f7bcf74bbaf3be646fa4dcd31fc32bcd75a8c421
Vlad S <ushulau@gmail.com>
2017-11-05 15:19:34 -0500

    - rewired to run as a console app

--- src/main/java/com/gplex/open/trader/Application.java
+++ src/main/java/com/gplex/open/trader/Application.java
@@ -3,25 +3,44 @@ package com.gplex.open.trader;
 import com.gplex.open.trader.constant.Intervals;
 import com.gplex.open.trader.constant.Products;
 import com.gplex.open.trader.engine.Engine;
+import com.gplex.open.trader.service.AccountsServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    @Autowired
+    private AccountsServiceImpl accountsService;
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        logger.debug("Done");
+
+    }
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        logger.debug("Starting engine");
         final int[] count = {0};
         Engine enginge = new Engine((engine, tickerMessage) -> {
-
+            if(count[0]%10 == 0){
+                logger.debug("{}", accountsService.listAccounts());
+            }
             count[0]++;
             logger.debug("{}", count[0]);
 
 
 
         }, Products.LTC_USD, Intervals._15_S, Intervals._1_M);
+
+        //do something
+
     }
+
 }
--- src/main/java/com/gplex/open/trader/config/RootConfig.java
+++ src/main/java/com/gplex/open/trader/config/RootConfig.java
@@ -1,7 +1,10 @@
 package com.gplex.open.trader.config;
 
+import com.gplex.open.trader.service.AccountsServiceImpl;
+import com.gplex.open.trader.utils.Security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,15 +15,30 @@ import org.springframework.web.client.RestTemplate;
 public class RootConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(RootConfig.class);
 
-
-    /*@Value("${gdax.auth.endpoint}")
-    private String gdaxAuthEndpont;*/
+    @Value("${gdax.secret}")
+    private String privateKey;
+    @Value("${gdax.key}")
+    private String key;
+    @Value("${gdax.passphrase}")
+    private String passphrase;
+    @Value("${gdax.api.baseUrl}")
+    private String baseUrl;
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
+
+    @Bean
+    public Security security() {
+        return new Security(privateKey);
+    }
+
+    @Bean
+    public AccountsServiceImpl accountsService(RestTemplate rt, Security sec) {
+        return new AccountsServiceImpl(rt, sec, key, passphrase, baseUrl);
+    }
 
 }
 
